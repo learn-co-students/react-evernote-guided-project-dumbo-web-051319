a6f6fd3e99fdae718073b48e9c7ab84809bbbd51
Joseph G Syverson <josephgavinsyverson@gmail.com>
2019-07-25 10:16:11 -0400

    proxy | first fetch to App state

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,6 +3,19 @@ import Header from './Header';
 import NoteContainer from './NoteContainer';
 
 class App extends Component {
+
+  state = {
+    notes: []
+  }
+
+  componentDidMount() {
+    // debugger
+    fetch('/api/v1/notes')
+      .then((resp) => resp.json())
+        .then((json) => this.setState({notes: json}))
+        // .then((json) => console.log(jso))
+  }
+
   render() {
     return (
       <div className="app">
@@ -11,6 +24,7 @@ class App extends Component {
       </div>
     );
   }
+
 }
 
 export default App;
